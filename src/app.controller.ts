b61d5c883107c584e1fc122069fb12f84bc501b4
Klement Szabolcs <klementszabolcs1@gmail.com>
2022-12-07 14:01:14 +0100

    Initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Get, Param, Query, Render } from '@nestjs/common';
+import { AppService } from './app.service';
+import db from './db';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @Render('list')
+  async listPaintings(@Query('year') year = 1990) {
+    const [ rows ] = await db.execute(
+      'SELECT title FROM paintings WHERE year > ?',
+      [ year ]
+      );
+
+
+
+    return {
+      paintings: rows,
+    };
+  }
+
+  @Get('paintings/:id')
+  async showPainting(@Param('id') id: number){
+    const [ rows ] = await db.execute(
+      'SELECT title, year, on_display FROM paintings WHERE id = ?',
+      [ id ]
+    );
+    return { paintings: rows[0] };
+  }
+}
